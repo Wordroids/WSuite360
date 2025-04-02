90b8c36dea051da7670aa505e5fdce05c052cd0f
ChethanaWijesinghe10 <chethanawijesinghe10@gmail.com>
2025-04-02 13:17:55 +0530

    Updated Time Entry Approval Screens and Activity Log Screen

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -115,19 +115,19 @@
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="#"
+                                    <a href="{{ route('pages.activity_log.logs') }}"
                                         class="group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-indigo-200 hover:bg-indigo-700 hover:text-white">
                                         <span
                                             class="flex size-6 shrink-0 items-center justify-center rounded-lg border border-indigo-400 bg-indigo-500 text-[0.625rem] font-medium text-white">T</span>
-                                        <span class="truncate">Tailwind Labs</span>
+                                        <span class="truncate">Activity Log</span>
                                     </a>
                                 </li>
                                 <li>
-                                    <a href="#"
+                                    <a href="{{ route('pages.time_entry_approval.index') }}"
                                         class="group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-indigo-200 hover:bg-indigo-700 hover:text-white">
                                         <span
                                             class="flex size-6 shrink-0 items-center justify-center rounded-lg border border-indigo-400 bg-indigo-500 text-[0.625rem] font-medium text-white">W</span>
-                                        <span class="truncate">Workcation</span>
+                                        <span class="truncate">Time Entry Approval</span>
                                     </a>
                                 </li>
                             </ul>
@@ -256,19 +256,19 @@
                             </a>
                         </li>
                         <li>
-                            <a href="#"
+                            <a href="{{ route('pages.activity_log.logs') }}"
                                 class="group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-indigo-200 hover:bg-indigo-700 hover:text-white">
                                 <span
                                     class="flex size-6 shrink-0 items-center justify-center rounded-lg border border-indigo-400 bg-indigo-500 text-[0.625rem] font-medium text-white">T</span>
-                                <span class="truncate">Tailwind Labs</span>
+                                <span class="truncate">Activity Log</span>
                             </a>
                         </li>
                         <li>
-                            <a href="#"
+                            <a href="{{ route('pages.time_entry_approval.index') }}"
                                 class="group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-indigo-200 hover:bg-indigo-700 hover:text-white">
                                 <span
                                     class="flex size-6 shrink-0 items-center justify-center rounded-lg border border-indigo-400 bg-indigo-500 text-[0.625rem] font-medium text-white">W</span>
-                                <span class="truncate">Workcation</span>
+                                <span class="truncate">Time Entry Approval</span>
                             </a>
                         </li>
                     </ul>
--- routes/tenant.php
+++ routes/tenant.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\ActivityLogController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\Auth\ConfirmablePasswordController;
 use App\Http\Controllers\Auth\EmailVerificationNotificationController;
@@ -23,6 +24,7 @@ use App\Http\Controllers\EmployeeDashboardController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TimeEntryApprovalController;
 use App\Http\Controllers\TimeLogController;
 use App\Http\Controllers\TimeLogApprovalController;
 use App\Http\Controllers\TimeSheetController;
@@ -130,6 +132,14 @@ Route::middleware([
             Route::resource('clients', ClientController::class);
         });
 
+        //Activity log
+        Route::get('/logs', [ActivityLogController::class, 'logs'])->name('pages.activity_log.logs');
+
+        //Time Entry Approval
+        Route::get('/index', [TimeEntryApprovalController::class, 'index'])->name('pages.time_entry_approval.index');
+        Route::get('/pending', [TimeEntryApprovalController::class, 'pending'])->name('pages.time_entry_approval.pending');
+        Route::get('/approved', [TimeEntryApprovalController::class, 'approved'])->name('pages.time_entry_approval.approved');
+        Route::get('/rejected', [TimeEntryApprovalController::class, 'rejected'])->name('pages.time_entry_approval.rejected');
         // Employee Routes (Time Logs & Break Logs)
         Route::middleware('role:developer')->group(function () {
             Route::resource('time_logs', TimeLogController::class)->except(['destroy']);
