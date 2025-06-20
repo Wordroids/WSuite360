17b83d1fc89c27b39cd409542570c9dc11baca17
ChethanaWijesinghe10 <chethanawijesinghe10@gmail.com>
2025-06-20 12:54:33 +0530

    UI Enhancements.

--- resources/views/pages/payments/cardConfirm.blade.php
+++ resources/views/pages/payments/cardConfirm.blade.php
@@ -37,7 +37,7 @@
             </div>
 
             <div class="mt-6 flex space-x-4">
-                <button type="submit" class="flex-1 px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">
+                <button type="submit" class="flex-1 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
                     Confirm Payment
                 </button>
                 <a href="{{ route('payments.card-charge') }}"
