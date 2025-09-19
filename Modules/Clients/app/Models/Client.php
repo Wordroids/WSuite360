93fad67f65b444248be0c03ffef03f981606aaf0
ChethanaWijesinghe10 <chethanawijesinghe10@gmail.com>
2025-09-19 11:04:38 +0530

    Modularize Projects.

--- Modules/Clients/app/Models/Client.php
+++ Modules/Clients/app/Models/Client.php
@@ -6,8 +6,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-use App\Models\Project;
 use App\Models\ProjectSubscription;
+use Modules\Projects\Models\Project;
+
 class Client extends Model
 {
     /** @use HasFactory<\Database\Factories\ClientFactory> */
